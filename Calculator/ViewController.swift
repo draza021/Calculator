a47b21d6138ce2952558d06f28ed690635432a48
Dragoslav Ignjatovic <draza021@gmail.com>
2018-04-10 17:49:07 +0200

    added zero button frame measurement

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnZero: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let insetValue = btnZero.frame.size.width / 2
+        print("viewDIdAppear")
+        print(insetValue)
+        
+        btnZero.contentEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: insetValue)
     }
-
-
+  
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        coordinator.animate(alongsideTransition: nil) { [unowned self] btnZero in
+            let insetValue = self.btnZero.frame.size.width / 2
+            print("viewWillTransition")
+            print(insetValue)
+            
+            self.btnZero.contentEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: insetValue)
+        }
+    }
 }
 
